4d8561e57bfb76e4d9ecb236943c274e5368180e
Thomas Volk <tvolk131@gmail.com>
2018-04-25 18:08:10 -0500

    Added white card deck

---
+++ src/main/kotlin/game/WhiteCardDeck.kt
@@ -0,0 +1,35 @@
+package game
+
+class WhiteCardDeck(cards: List<WhiteCard>) {
+
+    private var drawPile: MutableList<WhiteCard> = cards.toMutableList()
+    private var discardPile: MutableList<WhiteCard> = ArrayList()
+
+    init {
+        drawPile.shuffle()
+    }
+
+    fun drawCard(): WhiteCard {
+        if (drawPile.isEmpty() && discardPile.isEmpty()) {
+            throw Exception("No cards are left in the deck")
+        }
+
+        if (drawPile.isEmpty()) {
+            reset()
+        }
+        val card = drawPile[0]
+        drawPile = drawPile.drop(1).toMutableList()
+        return card
+    }
+
+    fun discardCard(card: WhiteCard) {
+        discardPile.add(card)
+    }
+
+    fun reset() {
+        drawPile.addAll(discardPile)
+        discardPile = ArrayList()
+        drawPile.shuffle()
+    }
+
+}
---
+++ src/test/kotlin/WhiteCardDeckTest.kt
@@ -0,0 +1,104 @@
+import game.BlackCard
+import game.BlackCardDeck
+import game.WhiteCard
+import game.WhiteCardDeck
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class WhiteCardDeckTest {
+
+    private var cards: MutableList<WhiteCard> = ArrayList()
+    private var deck: WhiteCardDeck
+
+    init {
+        fillCardList()
+        deck = WhiteCardDeck(cards)
+    }
+
+    private fun fillCardList() {
+        for (i in 1..100) {
+            cards.add(TestWhiteCard(i.toString(), "1", i.toString()))
+        }
+    }
+
+    @BeforeEach
+    fun reset() {
+        cards = ArrayList()
+        fillCardList()
+        deck = WhiteCardDeck(cards)
+    }
+
+    @Test
+    fun doesNotModifyOriginalList() {
+        val cardsCopy = cards.toList()
+        val size = cards.size
+
+        for (i in 1..5) {
+            deck.discardCard(deck.drawCard())
+        }
+
+        assertEquals(size, cards.size)
+        assert(cards.toTypedArray().contentDeepEquals(cardsCopy.toTypedArray()))
+    }
+
+    @Test
+    fun cyclesThroughAllCards() {
+        for (i in 1..10) {
+            val cardIds: MutableSet<String> = HashSet()
+
+            for (j in 1..100) {
+                val card = deck.drawCard()
+                assert(!cardIds.contains(card.id))
+                cardIds.add(card.id)
+                deck.discardCard(card)
+            }
+
+            for (j in 1..100) {
+                assert(cardIds.contains(i.toString()))
+            }
+        }
+    }
+
+    @Test
+    fun errorsOnEmptyDeck() {
+        for (i in 1..100) {
+            deck.drawCard()
+        }
+
+        val e = assertThrows(Exception::class.java, { deck.drawCard() })
+        assertEquals("No cards are left in the deck", e.message)
+    }
+
+    @Test
+    fun recyclesCards() {
+        for (i in 1..10) {
+            val cards: MutableList<WhiteCard> = ArrayList()
+            for (j in 1..100) {
+                cards.add(deck.drawCard())
+            }
+
+            cards.forEach { card -> deck.discardCard(card) }
+        }
+    }
+
+    @Test
+    fun resetDeck() {
+        for (i in 1..50) {
+            deck.discardCard(deck.drawCard())
+        }
+        deck.reset()
+
+        cyclesThroughAllCards()
+    }
+
+
+    private class TestWhiteCard(
+            override val id: String,
+            override val cardpackId: String,
+            override val text: String
+    ) : WhiteCard
+
+}
