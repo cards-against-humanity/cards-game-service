dc83f2a2f89ee9a093d87731bdcb2e40f84dd467
Thomas Volk <tvolk131@gmail.com>
2018-04-30 23:23:15 -0500

    Further implemented game logic

--- src/main/kotlin/game/Game.kt
+++ src/main/kotlin/game/Game.kt
@@ -1,48 +1,82 @@
 package game
 
-import java.util.*
+class Game(private var maxPlayers: Int, whiteCards: List<WhiteCard>, blackCards: List<BlackCard> /* TODO - Add socket handler as arg */) {
+    private val handSize = 4
 
-class Game(internal var maxPlayers: Int, whiteCards: List<WhiteCard>, blackCards: List<BlackCard> /* TODO - Add socket handler as arg */) {
-    private var players: MutableList<Player> = ArrayList()
-    private var ownerId: String? = null
-    private var judgeId: String? = null
     private var stage: GameStage = GameStage.NOT_RUNNING
-    private var nextStage: Date? = null
-    private var timer: Timer? = null
-    private var whitePlayed: Map<Int, Array<WhiteCard>>? = null
+    private var whitePlayed: MutableMap<String, MutableList<WhiteCard>> = HashMap()
     private var whiteDeck = WhiteCardDeck(whiteCards)
     private var blackDeck = BlackCardDeck(blackCards)
+    private val playerManager: PlayerManager = PlayerManager(handSize, whiteDeck)
 
     private val isRunning: Boolean
         get() = stage != GameStage.NOT_RUNNING
 
+    private val ownerId: String?
+        get() = if (playerManager.owner != null) { playerManager.owner!!.id } else { null }
+
+    private val judgeId: String?
+        get() = if (playerManager.judge != null) { playerManager.judge!!.id } else { null }
+
+    private val players: Map<String, Player>
+        get() = playerManager.players
+
+    init {
+        val minCardCount = maxPlayers * (handSize + 4)
+        if (maxPlayers < 3) {
+            throw Exception("Max players must be at least 3")
+        }
+        if (maxPlayers > 20) {
+            throw Exception("Max players cannot be greater than 20")
+        }
+        if (whiteCards.size < minCardCount) {
+            throw Exception("Not enough white cards, need ${minCardCount - whiteCards.size} more with this many players")
+        }
+    }
 
     fun start(userId: String) {
-        if (userId != judgeId) {
+        if (userId != ownerId) {
             throw InsufficientAccessException("Must be owner to start game")
         } else if (isRunning) {
             throw Exception("Game is already running")
         } else if (players.size < 3) {
             throw Exception("Must have at least 3 players to start game")
         }
-        TODO()
+
+        stage = GameStage.PLAY_PLASE
     }
 
     fun stop(userId: String) {
-        if (userId != judgeId) {
+        if (userId != ownerId) {
             throw InsufficientAccessException("Must be owner to stop game")
         } else if (!isRunning) {
             throw Exception("Game is not running")
         }
-        TODO()
+
+        whitePlayed.forEach { cardList -> cardList.value.forEach { card -> whiteDeck.discardCard(card) } }
+        whitePlayed = HashMap()
+        whiteDeck.reset()
+        blackDeck.reset()
+        playerManager.reset()
+
+        stage = GameStage.NOT_RUNNING
     }
 
 
     fun join(userId: String) {
-        players.add(Player(userId))
+        if (players.size == maxPlayers) {
+            throw Exception("Game is full")
+        }
+        playerManager.addUser(userId)
+        whitePlayed[userId] = ArrayList()
     }
 
-    fun leave(userId: String) {}
+    fun leave(userId: String) {
+        // TODO - Check if user was judge and react accordingly
+        playerManager.removeUser(userId)
+        whitePlayed[userId]!!.forEach { card -> whiteDeck.discardCard(card) }
+        whitePlayed.remove(userId)
+    }
 
     fun kickUser(kickerId: String, kickeeId: String) {
         if (kickerId != ownerId) {
@@ -50,11 +84,36 @@ class Game(internal var maxPlayers: Int, whiteCards: List<WhiteCard>, blackCards
         } else if (kickerId == kickeeId) {
             throw InsufficientAccessException("Cannot kick yourself from the game")
         }
+        leave(kickeeId)
     }
 
-    fun playCard(userId: String, cardId: String) {}
+    fun playCard(userId: String, cardId: String) {
+        if (userId == judgeId) {
+            throw Exception("Judge cannot play a card")
+        } else if(stage != GameStage.PLAY_PLASE) {
+            throw Exception("Cannot play cards right now")
+        } else if (players[userId] == null) {
+            throw Exception("User is not in the game")
+        } else if (whitePlayed[userId]!!.size == blackDeck.currentCard.answerFields) {
+            throw Exception("You cannot play anymore cards for this round")
+        }
 
-    fun voteCard(userId: String, cardId: String) {}
+        whitePlayed[userId]!!.add(players[userId]!!.playCard(cardId))
+
+        if (!players.all { player -> whitePlayed[player.value.id]!!.size == blackDeck.currentCard.answerFields }) {
+            stage = GameStage.JUDGE_PHASE
+        }
+    }
+
+    fun voteCard(userId: String, cardId: String) {
+        if (userId != judgeId) {
+            throw Exception("Must be the judge to vote for a card")
+        } else if (stage != GameStage.JUDGE_PHASE) {
+            throw Exception("Cannot vote for a card")
+        }
+
+        // TODO - Set winner
+    }
 
     fun getFOV(userId: String): FOVGameInfo {
         TODO()
@@ -70,18 +129,4 @@ class Game(internal var maxPlayers: Int, whiteCards: List<WhiteCard>, blackCards
     class FOVGameInfo
 
     class Info
-
-    class Player(userId: String) {
-
-        val id: String = userId
-
-        var score = 0
-            private set
-
-        fun incrementScore() {
-            score++
-        }
-
-    }
-
 }
