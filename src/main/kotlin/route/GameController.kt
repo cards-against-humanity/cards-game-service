3796dd49edb504feb28e32beccfdd668272b277f
Thomas Volk <tvolk131@gmail.com>
2018-05-02 17:58:05 -0500

    Added routes for creating, joining, and leaving games

--- src/main/kotlin/route/GameController.kt
+++ src/main/kotlin/route/GameController.kt
@@ -1,5 +1,6 @@
 package route
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import game.GameManager
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
@@ -12,6 +13,34 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 class GameController(private val gameManager: GameManager) {
+
+    @RequestMapping(value = "/{userId}/game/create", method = [RequestMethod.POST])
+    @ApiOperation(value = "Create game")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "Game successfully created")
+    )
+    fun createGame(@PathVariable userId: String, @RequestBody gameData: CreateGameData): ResponseEntity<FOVGameData> {
+        return ResponseEntity.ok(gameManager.createGame(userId, gameData.gameName, gameData.maxPlayers, gameData.cardpackIds))
+    }
+
+    @RequestMapping(value = "/{userId}/game/{gameName}/join", method = [RequestMethod.POST])
+    @ApiOperation(value = "Join game")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "Successfully joined game")
+    )
+    fun joinGame(@PathVariable userId: String, @PathVariable gameName: String): ResponseEntity<FOVGameData> {
+        return ResponseEntity.ok(gameManager.joinGame(userId, gameName))
+    }
+
+    @RequestMapping(value = "/{userId}/game", method = [RequestMethod.DELETE])
+    @ApiOperation(value = "Leave game")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "Successfully left game")
+    )
+    fun leaveGame(@PathVariable userId: String): ResponseEntity<Any> {
+        gameManager.leaveGame(userId)
+        return ResponseEntity.noContent().build()
+    }
 
     @RequestMapping(value = "/games", method = [RequestMethod.GET])
     @ApiOperation(value = "Get a list of all running games")
@@ -68,5 +97,11 @@ class GameController(private val gameManager: GameManager) {
             ResponseEntity.status(HttpStatus.FORBIDDEN).build()
         }
     }
+
+    data class CreateGameData(
+            @JsonProperty("gameName") val gameName: String,
+            @JsonProperty("maxPlayers") val maxPlayers: Int,
+            @JsonProperty("cardpackIds") val cardpackIds: List<String>
+    )
 
 }
