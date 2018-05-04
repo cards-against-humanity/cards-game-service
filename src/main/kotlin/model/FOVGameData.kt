529e84a38d4254ee13b90c196f4596f9701d7043
Thomas Volk <tvolk131@gmail.com>
2018-05-03 19:41:25 -0500

    Added properties to fovgamedata

--- src/main/kotlin/model/FOVGameData.kt
+++ src/main/kotlin/model/FOVGameData.kt
@@ -1,3 +1,12 @@
 package model
 
-interface FOVGameData
+data class FOVGameData(
+        val name: String,
+        val maxPlayers: Int,
+        val hand: List<WhiteCard>,
+        val players: List<FOVPlayer>,
+        val judgeId: String,
+        val ownerId: String,
+        val whitePlayed: Map<String, List<WhiteCard?>>,
+        val currentBlackCard: BlackCard?
+)
