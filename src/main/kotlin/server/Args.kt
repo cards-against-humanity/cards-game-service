180dfe1746d6987f28f3bfd707bb50b5284e77b8
Thomas Volk <tvolk131@gmail.com>
2018-05-05 17:48:49 -0500

    Added http security arg

--- src/main/kotlin/server/Args.kt
+++ src/main/kotlin/server/Args.kt
@@ -4,5 +4,6 @@ import java.net.URL
 
 open class Args {
     val allowedCorsOrigin: String = System.getenv("ALLOWED_CORS_ORIGIN") ?: "http://localhost"
-    val apiUrl: URL = URL(System.getenv("API_URL")!!)
+    val apiUrl: URL = URL(System.getenv("API_URL") ?: throw Exception("Missing environment variable: API_URL"))
+    val secureApiConnection: Boolean = try { System.getenv("SECURE_API_CONNECTION")!!.toBoolean() } catch (e: Exception) { true }
 }
