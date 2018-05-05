fb9327fc70425ca1792748f3b8b8d5db144bd676
Thomas Volk <tvolk131@gmail.com>
2018-05-05 15:09:57 -0500

    Added api url arg

--- src/main/kotlin/server/Args.kt
+++ src/main/kotlin/server/Args.kt
@@ -1,5 +1,8 @@
 package server
 
+import java.net.URL
+
 open class Args {
     val allowedCorsOrigin: String = System.getenv("ALLOWED_CORS_ORIGIN") ?: "http://localhost"
+    val apiUrl: URL = URL(System.getenv("API_URL")!!)
 }
