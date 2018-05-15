6fbfa61379876bcb1da6cc955ac8ee442a51b6a3
Thomas Volk <tvolk131@gmail.com>
2018-05-14 21:29:01 -0500

    White deck completely seales off all white cards

--- src/test/kotlin/game/gamelogic/WhiteCardDeckTest.kt
+++ src/test/kotlin/game/gamelogic/WhiteCardDeckTest.kt
@@ -13,7 +13,7 @@ class WhiteCardDeckTest {
 
     init {
         fillCardList()
-        deck = WhiteCardDeck(cards)
+        deck = WhiteCardDeck(cards, 8)
     }
 
     private fun fillCardList() {
@@ -26,7 +26,7 @@ class WhiteCardDeckTest {
     fun reset() {
         cards = ArrayList()
         fillCardList()
-        deck = WhiteCardDeck(cards)
+        deck = WhiteCardDeck(cards, 8)
     }
 
     @Test
@@ -35,61 +35,11 @@ class WhiteCardDeckTest {
         val size = cards.size
 
         for (i in 1..5) {
-            deck.discardCard(deck.drawCard())
+            deck.addUser(i.toString())
         }
 
         assertEquals(size, cards.size)
         assert(cards.toTypedArray().contentDeepEquals(cardsCopy.toTypedArray()))
-    }
-
-    @Test
-    fun cyclesThroughAllCards() {
-        for (i in 1..10) {
-            val cardIds: MutableSet<String> = HashSet()
-
-            for (j in 1..100) {
-                val card = deck.drawCard()
-                assert(!cardIds.contains(card.id))
-                cardIds.add(card.id)
-                deck.discardCard(card)
-            }
-
-            for (j in 1..100) {
-                assert(cardIds.contains(i.toString()))
-            }
-        }
-    }
-
-    @Test
-    fun errorsOnEmptyDeck() {
-        for (i in 1..100) {
-            deck.drawCard()
-        }
-
-        val e = assertThrows(Exception::class.java, { deck.drawCard() })
-        assertEquals("No cards are left in the deck", e.message)
-    }
-
-    @Test
-    fun recyclesCards() {
-        for (i in 1..10) {
-            val cards: MutableList<WhiteCard> = ArrayList()
-            for (j in 1..100) {
-                cards.add(deck.drawCard())
-            }
-
-            cards.forEach { card -> deck.discardCard(card) }
-        }
-    }
-
-    @Test
-    fun resetDeck() {
-        for (i in 1..50) {
-            deck.discardCard(deck.drawCard())
-        }
-        deck.reset()
-
-        cyclesThroughAllCards()
     }
 
 
